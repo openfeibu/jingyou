9224db1a9250311dcdf00af84c23e29780866acc
wzj <wzj@3b62d73a-4543-184b-9f22-58ddad37e77e>
2018-09-02 13:26:40 +0000

    git-svn-id: https://39.108.144.9:8443/svn/jingyou@10 3b62d73a-4543-184b-9f22-58ddad37e77e

--- public/themes/admin/partials/header.blade.php
+++ public/themes/admin/partials/header.blade.php
@@ -9,7 +9,7 @@
         <li class="layui-nav-item" lay-unselect="">
             <a href="javascript:;"><img src="{!!user()->picture!!}" class="layui-nav-img">{!!user()->name!!}</a>
             <dl class="layui-nav-child">
-                <dd><a href="javascript:;">修改信息</a></dd>
+                <dd><a href="{{ guard_url('password') }}">修改信息</a></dd>
                 <dd><a href="{{ guard_url('logout') }}">退出</a></dd>
             </dl>
         </li>
